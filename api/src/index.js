364a1b767df8e426c800fc7f9f57f679de0493e5
Afanaciev Kirill <afanasiev.kirill@gmail.com>
2020-06-17 21:14:49 +0300

    add db request

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,34 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const { connectDb } = require("./helpers/db");
+const {port, host} = require("./configuration");
 const app = express();
-const port = process.env.PORT;
-const host = process.env.HOST;
+const postSchema = new mongoose.Schema({
+    name: String
+  });
+const Post = mongoose.model('Post', postSchema);
+
+
+const startServer = ()=>{
+    app.listen(port, () => {
+        console.log('Started api service on port: ${ port }');
+        console.log('On host: ${ host }');
+        console.log('On host: ${ connectDb }');
+
+        const silence = new Post({ name: 'Silence' });
+        silence.save(function (err, savedSilence) {
+            if (err) return console.error(err);
+            console.log("Saved silence", savedSilence);
+          });
+        console.log(silence.name);
+    });
+};
 
 app.get("/test", (req, res) => {
     res.send("Service working correctly");
 });
 
-app.listen(port, () => {
-    console.log('Started api service on port: ${port}');
-    console.log('On host: ${host}');
-});
+connectDb()
+    .on('error', console.log)
+    .on('disconnected', connectDb)
+    .once('open', startServer);
